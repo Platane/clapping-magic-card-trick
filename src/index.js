adddbbc519fe0ea154d3df1c75698dca5a66619e
Platane <me@platane.me>
2017-02-17 00:07:28 +0100

    :rocket: training mode

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-/* global AudioContext, navigator, a,b,c,d,  gain, context, analyser, phase, t, startDate, x, cards, mask */
+/* global AudioContext, navigator, a,b,c,d,  gain, context, analyser, phase, t, startDate, x, cards, mask, showTimeMode, _phase */
 
 
 const createCard = ( value, icon, color ) => {
     const el = d.createElement('div')
     el.innerHTML = value+' '+icon
 
-    el.setAttribute('style','transform-origin:50% 360px;transition:transform 300ms ease;padding:10px;background:#fff;position:absolute;top:50%;left:calc(50% - 50px);box-shadow:0 0 5px 0 #333;border-radius:10px;width:100px;height:140px;color:'+color)
+    el.setAttribute('style','transform-origin:50% 360px;transition:transform 400ms;padding:10px;background:#fff;position:absolute;top:50%;left:calc(50% - 50px);box-shadow:0 0 5px 0 #333;border-radius:10px;width:100px;height:140px;color:'+color)
 
     return el
 }
@@ -21,27 +21,20 @@ for (let i=8;i--;) {
 }
 
 
-phase = 0
+phase = -1
 gain = 0
 t = 0
 x = 0
 startDate = 0
-
-const demo = true
+showTimeMode = 0
 
 navigator.getUserMedia(
     { audio: true },
     stream => {
 
-        // creates the audio context
         context = new AudioContext()
 
-        // const processor = context.createScriptProcessor( 2048, 1, 1 )
         analyser = context.createAnalyser()
-        // analyser.smoothingTimeConstant = 1
-        // analyser.fftSize = 256
-        // analyser.minDecibels = -90
-        // analyser.maxDecibels = -10
 
         const bufferLength = analyser.frequencyBinCount
         const dataArray = new Uint8Array(bufferLength)
@@ -58,18 +51,6 @@ navigator.getUserMedia(
             analyser.getByteTimeDomainData(dataArray)
 
 
-            // ctx.clearRect(0,0,9999,9999)
-            //
-            // ctx.beginPath()
-            // ctx.moveTo(400,400)
-            // for (let i = bufferLength; i--;)
-            //     ctx.lineTo( 400*i/bufferLength, dataArray[i] * 400 / 128 )
-            //
-            // ctx.strokeStyle='#333'
-            // ctx.stroke()
-
-            c.clearRect(0,0,999,999)
-
 
 
             gain = gain*0.96
@@ -77,25 +58,40 @@ navigator.getUserMedia(
             for (let i = bufferLength; i--;)
                 gain = Math.max(gain, Math.abs(128-dataArray[i])*3.5)
 
-            c.beginPath()
-            c.rect(9,99,400,9)
-            c.stroke()
-            c.beginPath()
-            c.rect(9,99,8*gain,9)
-            c.fill()
+            c.clearRect(0,0,999,999)
 
-            if ( gain > 50 ){
+            if ( !showTimeMode ){
+
                 c.beginPath()
-                c.rect(99,199,50,50)
+                c.rect(9,99,400,9)
+                c.stroke()
+                c.beginPath()
+                c.rect(9,99,8*gain,9)
                 c.fill()
+
+                if ( gain > 50 ){
+                    c.beginPath()
+                    c.rect(99,199,50,50)
+                    c.fill()
+                }
+
+                for ( let i = 3; i--;  ){
+                    c.beginPath()
+                    c.rect(8+(i+1)*100,30,4, 10 + 20 * (i<phase)  )
+                    x&(1<<i) ? c.fill() : c.stroke()
+                }
+
+                if ( startDate ) {
+                    c.beginPath()
+                    c.rect(9,9,300,9)
+                    c.stroke()
+                    c.beginPath()
+                    c.rect(9,9,(t-startDate),9)
+                    c.fill()
+                }
             }
 
-            for ( let i = 3; i--;  ){
-                c.beginPath()
-                c.rect(8+(i+1)*100,30,4, 10 + 20 * (i<phase)  )
-                x&(1<<i) ? c.fill() : c.stroke()
-            }
-
+            _phase = phase
 
             if ( !startDate ){
                 if ( gain > 50 ) {
@@ -104,37 +100,29 @@ navigator.getUserMedia(
                 }
             } else {
 
-                c.beginPath()
-                c.rect(9,9,300,9)
-                c.stroke()
-                c.beginPath()
-                c.rect(9,9,(t-startDate),9)
-                c.fill()
-
                 if ( (t-startDate) == (phase+1)*100 ) {
                     if ( phase == 3 )
                         return cards[x].style.transform = 'scale(2)'
 
                     x += ( 1<<phase ) * ( gain > 50 )
-
-                    mask = (1<<(phase+1))-1
-
-                    for(let i=8;i--;)
-                        cards[i].style.transform = (x & mask) != (i & mask)
-                            ? 'scale(0.5) translate3d(0,200px,0)'
-                            : ( (1<<(phase+1)) & i )
-                                ?   'translate3d('+(-i*30)+'px,-100px,0)'
-                                :   'translate3d('+(-i*30)+'px,100px,0)'
-
                     phase ++
                 }
             }
+
+            if ( _phase != phase && !showTimeMode )
+                for(let i=8;i--;)
+                    cards[i].style.transform = (x & ((1<<(phase))-1)) != (i & ((1<<(phase))-1))
+                        ? 'scale(0.5) translate3d(0,200px,0)'
+                        : ( (1<<(phase)) & i )
+                            ?   'translate3d('+(-i*30)+'px,-100px,0)'
+                            :   'translate3d('+(-i*30)+'px,100px,0)'
 
             requestAnimationFrame( loop )
         }
 
 
         b.onkeyup = () => {
+            showTimeMode=true
             for(let i=8;i--;)
                 cards[i].style.transform = 'translate3d(0,200px,0)'
         }
